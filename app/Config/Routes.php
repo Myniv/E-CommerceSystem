1928953f94e18d9b9ca0d36246a61ea6a253033e
Mulyana N <mulyanan.ur26@gmail.com>
2025-02-05 18:22:19 +0700

    Insert view to routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Controllers\ProductController;
 use CodeIgniter\Router\RouteCollection;
 
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/product', [ProductController::class, 'allProduct']);
+$routes->get('/product/detail/(:num)', [ProductController::class, 'detailProduct']);
+$routes->get('/product/create', [ProductController::class, 'goCreateProduct']);
+$routes->post('/product/add', [ProductController::class, 'createProduct']);
+$routes->get('/product/edit/(:num)', [ProductController::class, 'goEditProduct']);
+$routes->post('/product/edit', [ProductController::class, 'editProduct']);
+$routes->delete('/product/delete/(:num)', [ProductController::class, 'deleteProduct']);
+
