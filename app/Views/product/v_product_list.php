d434af52900a7a4da38ad9ae01f8dc5fa8fe881f
Mulyana N <mulyanan.ur26@gmail.com>
2025-02-05 16:23:37 +0700

    Create product view list

---
+++ app/Views/product/v_product_list.php
@@ -0,0 +1,31 @@
+<h2>Produk List</h2>
+<a href="/prodcut/create">Tambah Mahasiswa</a>
+<table border="1">
+    <tr>
+        <th>Id</th>
+        <th>Nama</th>
+        <th>Harga</th>
+        <th>Stok</th>
+        <th>Kategori</th>
+        <th>Action</th>
+    </tr>
+    <?php foreach ($products as $product) { ?>
+        <tr>
+            <td><?= $product->getId(); ?></td>
+            <td><?= $product->getNama(); ?></td>
+            <td><?= $product->getHarga(); ?></td>
+            <td><?= $product->getStok(); ?></td>
+            <td><?= $product->getKategori(); ?></td>
+            <td>
+                <button onclick="location.href='/product/detail/<?= $mahasiswa->getNim(); ?>'">Detail</button>
+                <button onclick="location.href='/product/update/<?= $mahasiswa->getNim(); ?>'">Edit</button>
+                <form action="/product/delete/<?= $mahasiswa->getNim(); ?>" method="post" style="display:inline;">
+                    <input type="hidden" name="_method" value="DELETE">
+                    <button type="submit" onclick="return confirm('Apakah Anda yakin ingin menghapus produk ini?');">
+                        Hapus
+                    </button>
+                </form>
+            </td>
+        </tr>
+    <?php } ?>
+</table>
