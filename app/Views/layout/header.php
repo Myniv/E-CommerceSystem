d3fd97eb69ae98a12c7adf2b79568ab89ffcf5d1
Mulyana N <mulyanan.ur26@gmail.com>
2025-02-07 06:27:10 +0700

    Update header for nav link

--- app/Views/layout/header.php
+++ app/Views/layout/header.php
@@ -4,13 +4,14 @@
         <div class="container">
             <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                 <title>E-Commerce</title>
-                <h1 class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                <h1 class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0"
+                    onclick="window.location.href='<?= base_url('/') ?>';" style="cursor: pointer;">
                     E-Commerce
                 </h1>
                 <nav>
                     <a href="<?= base_url('/') ?>">Home</a>
-                    <a href="<?= base_url('/products') ?>">Products</a>
-                    <a href="<?= base_url('/contact') ?>">Contact</a>
+                    <a href="<?= base_url('/product') ?>">Products</a>
+                    <a href="<?= base_url('/pesanan') ?>">Pesanan</a>
                 </nav>
             </div>
         </div>
