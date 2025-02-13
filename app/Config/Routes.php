744eb6fe5446f13e2ee92baa2667305599e8d2ab
Mulyana N <mulyanan.ur26@gmail.com>
2025-02-13 15:54:26 +0700

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -3,30 +3,51 @@
 use App\Controllers\Home;
 use App\Controllers\PesananController;
 use App\Controllers\ProductController;
+use App\Controllers\UserController;
 use CodeIgniter\Router\RouteCollection;
 
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', [Home::class, 'index']);
+
+$routes->environment('development', static function ($routes) {
+    $routes->get('/', [Home::class, 'development'], ['as' => 'user_dashboard']);
+});
+$routes->environment('production', static function ($routes) {
+    $routes->get('/', [Home::class, 'production'], ['as' => 'user_dashboard']);
+});
+
 $routes->addRedirect('/home', '/');
 $routes->get('/about-us', [Home::class, 'aboutUs']);
 
-//Product Route
-$routes->get('/product', [ProductController::class, 'allProduct']);
-$routes->get('/product/detail/(:num)', [ProductController::class, 'detailProduct']);
-$routes->get('/product/create', [ProductController::class, 'goCreateProduct']);
-$routes->post('/product/add', [ProductController::class, 'createProduct']);
-$routes->get('/product/edit/(:num)', [ProductController::class, 'goEditProduct']);
-$routes->post('/product/edit', [ProductController::class, 'editProduct']);
-$routes->delete('/product/delete/(:num)', [ProductController::class, 'deleteProduct']);
+$routes->group('api/product', function ($routes) {
+    $routes->get('/', [ProductController::class, 'allProduct']);
+    $routes->get('detail/(:num)', [ProductController::class, 'detailProduct/$1'], ['as' => 'product_details']);
+    $routes->get('create', [ProductController::class, 'goCreateProduct']);
+    $routes->post('add', [ProductController::class, 'createProduct']);
+    $routes->get('edit/(:num)', [ProductController::class, 'goEditProduct/$1']);
+    $routes->post('edit', [ProductController::class, 'editProduct']);
+    $routes->delete('delete/(:num)', [ProductController::class, 'deleteProduct/$1']);
+});
 
-//Pesanan Route
-$routes->get('/pesanan', [PesananController::class, 'allPesanan']);
-$routes->get('/pesanan/create', [PesananController::class, 'goCreatePesanan']);
-$routes->post('/pesanan/add', [PesananController::class, 'createPesanan']);
-$routes->post('/pesanan/edit', [PesananController::class, 'editPesanan']);
-$routes->get('/pesanan/edit/(:num)', [PesananController::class, 'goEditPesanan']);
-$routes->delete('/pesanan/delete/(:num)', [PesananController::class, 'deletePesanan']);
-$routes->get('/pesanan/detail/(:num)', [PesananController::class, 'detailPesanan']);
+$routes->group('api/pesanan', function ($routes) {
+    $routes->get('/', [PesananController::class, 'allPesanan']);
+    $routes->get('create', [PesananController::class, 'goCreatePesanan']);
+    $routes->post('add', [PesananController::class, 'createPesanan']);
+    $routes->post('edit', [PesananController::class, 'editPesanan']);
+    $routes->get('edit/(:num)', [PesananController::class, 'goEditPesanan/$1']);
+    $routes->delete('delete/(:num)', [PesananController::class, 'deletePesanan/$1']);
+    $routes->get('detail/(:num)', [PesananController::class, 'detailPesanan/$1']);
+});
 
+$routes->group('admin/user', function ($routes) {
+    $routes->get('/', [UserController::class, 'index']);
+    $routes->get('profile/(:num)', [UserController::class, 'detail']);
+    $routes->match(['get', 'post'], 'create', [UserController::class, 'create']);
+    $routes->match(['get', 'put'], 'update/(:num)', [UserController::class, 'update/$1']);
+    $routes->delete('delete/(:num)', [UserController::class, 'delete']);
+});
+
+$routes->get('/health-check', function () {
+    return view('v_health_check');
+});
