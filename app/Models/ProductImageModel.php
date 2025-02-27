14e0887d44485074cb2142e801bf02c3447c5b03
Mulyana N <mulyanan.ur26@gmail.com>
2025-02-27 19:07:52 +0700

    Create product image model and entities for database product_images table

---
+++ app/Models/ProductImageModel.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ProductImageModel extends Model
+{
+    protected $table = 'product_images';
+    protected $primaryKey = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType = \App\Entities\ProductImage::class;
+    protected $useSoftDeletes = false;
+    protected $protectFields = true;
+    protected $allowedFields = [
+        'product_id',
+        'image_path',
+        'is_primary',
+    ];
+
+    protected bool $allowEmptyInserts = false;
+    protected bool $updateOnlyChanged = true;
+
+    protected array $casts = [];
+    protected array $castHandlers = [];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat = 'datetime';
+    protected $createdField = 'created_at';
+    protected $updatedField = 'updated_at';
+    protected $deletedField = 'deleted_at';
+
+    // Validation
+    protected $validationRules = [
+        'product_id' => 'required|integer',
+        'image_path' => 'required',
+        'is_primary' => 'required',
+    ];
+    protected $validationMessages = [
+        'product_id' => [
+            'required' => 'The product id field is required.',
+            'integer' => 'The product is must number.',
+        ],
+        'image_path' => [
+            'required' => 'The image path field is required.',
+        ],
+        'is_primary' => [
+            'required' => 'The is primary field is required.',
+        ],
+    ];
+    protected $skipValidation = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert = [];
+    protected $afterInsert = [];
+    protected $beforeUpdate = [];
+    protected $afterUpdate = [];
+    protected $beforeFind = [];
+    protected $afterFind = [];
+    protected $beforeDelete = [];
+    protected $afterDelete = [];
+}
