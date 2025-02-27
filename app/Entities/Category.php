f9f9eeb81712dbb6838c1d43e5f9ab417c6f6a9a
Mulyana N <mulyanan.ur26@gmail.com>
2025-02-27 19:07:21 +0700

    Create category model and entities for database categories table

---
+++ app/Entities/Category.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Entities;
+
+use CodeIgniter\Entity\Entity;
+
+class Category extends Entity
+{
+    protected $attributes = [
+        'id' => null,
+        'name' => null,
+        'description' => null,
+        'status' => null,
+        'created_at' => null,
+        'updated_at' => null
+    ];
+    protected $casts = [
+        'id' => 'integer',
+        'name' => 'string',
+        'description' => 'string',
+        'status' => 'string',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime'
+    ];
+}
---
+++ app/Models/CategoryModel.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class CategoryModel extends Model
+{
+    protected $table = 'categories';
+    protected $primaryKey = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType = \App\Entities\Category::class;
+    protected $useSoftDeletes = false;
+    protected $protectFields = true;
+    protected $allowedFields = [
+        'name',
+        'description',
+        'status',
+    ];
+
+    protected bool $allowEmptyInserts = false;
+    protected bool $updateOnlyChanged = true;
+
+    protected array $casts = [];
+    protected array $castHandlers = [];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat = 'datetime';
+    protected $createdField = 'created_at';
+    protected $updatedField = 'updated_at';
+    protected $deletedField = 'deleted_at';
+
+    // Validation
+    protected $validationRules = [
+        'name' => 'required|min_length[3]|max_length[255]',
+        'description' => 'required|min_length[3]|max_length[500]',
+        'status' => 'required|min_length[3]|max_length[255]',
+    ];
+    protected $validationMessages = [
+        'name' => [
+            'required' => 'The name field is required.',
+            'min_length' => 'The name must be at least 3 characters long.',
+            'max_length' => 'The name must not exceed 255 characters.'
+        ],
+        'description' => [
+            'required' => 'The description field is required.',
+            'min_length' => 'The description must be at least 3 characters long.',
+            'max_length' => 'The description must not exceed 500 characters.'
+        ],
+        'status' => [
+            'required' => 'The status field is required.',
+            'min_length' => 'The status must be at least 3 characters long.',
+            'max_length' => 'The status must not exceed 255 characters.'
+        ],
+    ];
+    protected $skipValidation = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert = [];
+    protected $afterInsert = [];
+    protected $beforeUpdate = [];
+    protected $afterUpdate = [];
+    protected $beforeFind = [];
+    protected $afterFind = [];
+    protected $beforeDelete = [];
+    protected $afterDelete = [];
+}
