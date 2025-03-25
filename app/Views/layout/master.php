96ab941911ff510288eef57bff7fbd51a8f04a18
Mulyana N <mulyanan.ur26@gmail.com>
2025-03-25 12:06:10 +0700

    Update master layout

--- app/Views/layout/master.php
+++ app/Views/layout/master.php
@@ -1,15 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php use Config\Roles; ?> 
+<?php use Config\Roles; ?>
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= base_url('css/style.css') ?>">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <title><?= $this->renderSection('title') ?? 'E-Commerce' ?></title>
+    <link rel="stylesheet" href="<?php echo base_url('css/bootstrap.css') ?>" type="text/css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+    <script type="text/javascript" src="<?php echo base_url('js/bootstrap.js') ?>"><</script>
     <script src="<?= base_url('js/pristine/dist/pristine.js') ?>" type="text/javascript"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <title><?= $this->renderSection('title') ?? 'E-Commerce' ?></title>
 
     <style>
         .sidebar {
